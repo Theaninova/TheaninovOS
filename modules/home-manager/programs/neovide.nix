d7ceb1e7be6a9f3f127a9557a83e3adba806fa7d
Thea Schöbl <dev@theaninova.de>
2024-05-24 12:45:13 +0200

    fix: neovide

--- modules/home-manager/programs/neovide.nix
+++ modules/home-manager/programs/neovide.nix
@@ -1,5 +1,24 @@
 { pkgs, ... }:
 {
   home.packages = [ pkgs.neovide ];
-  xdg.configFile."neovide/config.toml".source = ./neovide.toml;
+  xdg.configFile."neovide/config.toml".source = (pkgs.formats.toml { }).generate "neovide" {
+    maximized = false;
+    fork = true;
+    font = {
+      normal = [ "FiraCode Nerd Font" ];
+      size = 13;
+      edging = "subpixelantialias";
+      features."FiraCode Nerd Font" = [
+        "+zero"
+        "+onum"
+        "+ss04"
+        "+cv19"
+        "+cv23"
+        "+ss09"
+        "+ss06"
+        "+ss07"
+        "+ss10"
+      ];
+    };
+  };
 }
