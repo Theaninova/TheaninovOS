b01574822a0d9422f0df0d7d03eddf4aa85a5d2b
Thea Schöbl <dev@theaninova.de>
2025-02-18 12:48:16 +0100

    update system

--- modules/nixos/desktops/hyprland.nix
+++ modules/nixos/desktops/hyprland.nix
@@ -8,7 +8,6 @@
 
 let
   cfg = config.desktops.hyprland;
-  homeConfig = config.home-manager.users.${username};
 in
 {
   options.desktops.hyprland = {
@@ -50,14 +49,6 @@ in
           Wants = [ "network-online.target" ];
         };
       };
-      /*
-        programs.zsh.initExtraFirst = # sh
-        ''
-          if uwsm check may-start; then
-            exec uwsm start hyprland-uwsm.desktop > ${homeConfig.xdg.configHome}/uwsm.log 2> ${homeConfig.xdg.configHome}/uwsm.err
-          fi
-        '';
-      */
     };
 
     systemd.services = {
@@ -65,81 +56,6 @@ in
         after = [ "network-online.target" ];
         wants = [ "network-online.target" ];
       };
-      /*
-        greetd.serviceConfig = {
-          Type = "idle";
-          StandardInput = "tty";
-          StandardOutput = "journal";
-          StandardError = "journal";
-          TTYReset = "yes";
-          TTYVHangup = "no";
-          TTYVTDisallocate = "no";
-        };
-      */
-      /*
-        "uwsm-display-manager" = {
-          description = "UWSM Display Manager";
-          conflicts = [ "getty@tty1.service" ];
-          after = [
-            "systemd-user-sessions.service"
-            "plymouth-quit-wait.service"
-            "getty@tty1.service"
-          ];
-          serviceConfig = {
-            Type = "simple";
-            IgnoreSIGPIPE = "no";
-            SendSIGHUP = "yes";
-            TimeoutStopSec = "30s";
-            KeyringMode = "shared";
-            Restart = "always";
-            RestartSec = "5";
-            StartLimitBurst = "5";
-            StartLimitInterval = "30";
-          };
-          restartIfChanged = false;
-          script = ''
-            ${pkgs.sudo}/bin/sudo -u ${username} --login ${
-              lib.getExe (
-                pkgs.writeShellApplication {
-                  name = "start-hyprland";
-                  text = ''
-                    ${pkgs.kbd}/bin/chvt 2
-                    exec ${pkgs.util-linux}/bin/agetty -o '-p -f -- \u' --noclear --autologin ${username} %I "$TERM" StandardOutput=null StandardError=journal
-                  '';
-                }
-              )
-            }
-          '';
-          aliases = [ "display-manager.service" ];
-        };
-      */
-      /*
-        "hyprtty" = {
-          description = "Start Hyprland";
-          conflicts = [ "getty@tty1.service" ];
-          after = [
-            "sysinit.target"
-            "initrd-switch-root.service"
-            "systemd-udev-trigger.service"
-            "seatd.service"
-            "systemd-logind.service"
-          ];
-          wants = [
-            "systemd-udev-trigger.service"
-            "seatd.service"
-            "systemd-logind.service"
-          ];
-          unitConfig.ConditionPathExists = "/home/${username}";
-          onFailure = [ "emergency.target" ];
-          serviceConfig.Type = "simple";
-          preStart = "+${pkgs.coreutils}/bin/rm -f /run/nologin";
-          script = "-${pkgs.util-linux}/bin/agetty -o '-p -f -- \\u' --noclear --autologin ${username} %I $TERM StandardOutput=null StandardError=journal";
-          wantedBy = [
-            "graphical.target"
-            "initrd-switch-root.service"
-          ];
-        };
-      */
     };
 
     xdg.portal = {
@@ -155,33 +71,11 @@ in
       };
     };
 
-    # https://github.com/sjcobb2022/nixos-config/blob/70fb548b961c19e9855d2de86ee9569a7a88d976/hosts/common/optional/greetd.nix#L23C1-L33C2
-    /*
-      systemd.services = {
-        greetd.serviceConfig = {
-          Type = "idle";
-          StandardInput = "tty";
-          StandardOutput = "tty";
-          StandardError = "journal"; # Without this errors will spam on screen
-          # Without these bootlogs will spam on screen
-          TTYReset = true;
-          TTYVHangup = true;
-          TTYVTDisallocate = true;
-        };
-      };
-    */
-
     services = {
       seatd = {
         enable = true;
         user = username;
       };
-      /*
-        getty = {
-          autologinUser = username;
-          extraArgs = [ "--noclear" ];
-        };
-      */
       kmscon = {
         enable = true;
         hwRender = true;
@@ -189,31 +83,14 @@ in
       greetd = {
         enable = true;
         greeterManagesPlymouth = false;
-        #vt = 1;
         settings = {
           initial_session = {
-            command = "uwsm start ${pkgs.hyprland}/share/wayland-sessions/hyprland.desktop" # > ${homeConfig.xdg.configHome}/uwsm.log 2> ${homeConfig.xdg.configHome}/uwsm.err"
-            /*
-              lib.getExe (
-                pkgs.writeShellApplication {
-                  name = "start-hyprland";
-                  text = ''
-                    chvt 1
-                    exec uwsm start hyprland-uwsm.desktop > ${homeConfig.xdg.configHome}/uwsm.log 2> ${homeConfig.xdg.configHome}/uwsm.err
-                  '';
-                }
-              )
-            */
-            ;
+            command = "uwsm start ${pkgs.hyprland}/share/wayland-sessions/hyprland.desktop";
             user = username;
           };
           default_session = {
             command = "${lib.getExe pkgs.greetd.tuigreet} --asterisks --remember --user-menu --cmd 'uwsm start hyprland-uwsm.desktop'";
             user = username;
-          };
-          terminal = {
-            #vt = lib.mkForce 2;
-            #switch = false;
           };
         };
       };
@@ -222,7 +99,6 @@ in
         implementation = "broker";
       };
       pcscd.enable = true;
-      # nautilus on non-gnome
       gvfs.enable = true;
     };
     programs.dconf.enable = true;
